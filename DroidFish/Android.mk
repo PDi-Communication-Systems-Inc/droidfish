f356fc495ab705e899c91b6ce443d4bc5e770eb1
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-06-01 16:24:04 -0400

    Changes to compile under AOSP, also was trying to compile cuckoochess app instead of just droidfish

--- DroidFish/Android.mk
+++ DroidFish/Android.mk
@@ -10,14 +10,12 @@ LOCAL_PACKAGE := droidfish
 
 # Specify java files to use in build
 LOCAL_SRC_FILES := $(call all-java-files-under)
-LOCAL_SRC_FILES += $(call all-java-files-under,../CuckooChess)
-LOCAL_SRC_FILES += $(call all-java-files-under,../CuckooChessAPK)
 LOCAL_SRC_FILES += $(call all-java-files-under,../CuckooChessEngine)
 LOCAL_SRC_FILES += $(call all-java-files-under,../CuckooChessEngineTest)
 LOCAL_SRC_FILES += $(call all-java-files-under,../DroidFishTest)
 
 # Java support libraries
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 junit-4.12.jar
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 junit-4.12
 
 # Give package a simple name
 LOCAL_PACKAGE_NAME := droidfish
@@ -28,5 +26,17 @@ LOCAL_MODULE_TAGS := optional
 # ensure signing with PDi key for distribution release
 LOCAL_CERTIFICATE := platform
 
+# bring in the resources
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay --generate-dependencies
+
 # tell build system to put the package together
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := junit-4.12:libs/junit-4.12.jar \
+					android-support-v4:libs/android-support-v4.jar
+
+include  $(BUILD_MULTI_PREBUILT)
